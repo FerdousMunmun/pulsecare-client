91a1206f8251e95d5f3e1af8d53b4a10b91a9fdd
Jannatul Ferdous <ferdousmunmun75@gmail.com>
2026-06-18 01:09:31 +0600

    navber page create

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -16,16 +16,18 @@ import {
 import { redirect } from "next/navigation";
 import React from "react";
 
-export default function SignInPage() {
+export default function SignUpPage() {
   const onSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const user = Object.fromEntries(formData.entries());
 
-    await authClient.signIn.email({
+    await authClient.signUp.email({
       ...user,
-      callbackURL: "/",
+      plan: 'free',
     });
+
+    redirect('/')
   };
 
   return (
@@ -36,6 +38,17 @@ export default function SignInPage() {
             <Fieldset.Legend>Signup</Fieldset.Legend>
             <Description>Create your account</Description>
             <Fieldset.Group>
+              <TextField isRequired name="name">
+                <Label>Name</Label>
+                <Input placeholder="John Doe" variant="secondary" />
+                <FieldError />
+              </TextField>
+
+              <TextField name="image" type="url">
+                <Label>Image URL</Label>
+                <Input placeholder="Image URL" variant="secondary" />
+                <FieldError />
+              </TextField>
               <TextField isRequired name="email" type="email">
                 <Label>Email</Label>
                 <Input placeholder="john@example.com" variant="secondary" />
@@ -47,10 +60,30 @@ export default function SignInPage() {
                 <Input placeholder="Password" variant="secondary" />
                 <FieldError />
               </TextField>
+
+              <Select isRequired name="role" placeholder="Select one">
+                <Label>Signup As</Label>
+                <Select.Trigger>
+                  <Select.Value />
+                  <Select.Indicator />
+                </Select.Trigger>
+                <Select.Popover>
+                  <ListBox>
+                    <ListBox.Item id="buyer" textValue="buyer">
+                      Buyer
+                      <ListBox.ItemIndicator />
+                    </ListBox.Item>
+                    <ListBox.Item id="seller" textValue="seller">
+                      Seller
+                      <ListBox.ItemIndicator />
+                    </ListBox.Item>
+                  </ListBox>
+                </Select.Popover>
+              </Select>
             </Fieldset.Group>
 
             <Button type="submit" className={"w-full"}>
-              Signin
+              Signup
             </Button>
           </Fieldset>
         </Form>
