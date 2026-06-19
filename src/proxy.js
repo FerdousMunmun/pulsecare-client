5f3de4f4fce99bca53fda44962cc6eeab8ba762a
Jannatul Ferdous <ferdousmunmun75@gmail.com>
2026-06-19 21:16:16 +0600

    proxy data added

--- src/proxy.js
+++ src/proxy.js
@@ -1,10 +1,21 @@
 import { NextResponse } from 'next/server'
+import { auth } from './lib/auth'
+import { headers } from 'next/headers'
  
 // This function can be marked `async` if using `await` inside
-export function proxy(request) {
-  return NextResponse.redirect(new URL('/home', request.url))
+export async function proxy(request) {
+const session = await auth.api.getSession({
+    headers: await headers()
+})
+ if(session?.user?.role == "donor" && session?.user?.plan === "free") {
+    return NextResponse.redirect(new URL('/funding', request.url))
+   }
+
+   if(!session){
+    return NextResponse.redirect(new URL('/signin', request.url))
+   }
 }
  
 export const config = {
-  matcher: '/about/:path*',
+  matcher: ['/profile' ,'/dashboard/donor']
 }
