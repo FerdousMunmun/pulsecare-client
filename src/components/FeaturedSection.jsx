bcc5596a564bb53f87fcb474c8a104433d9a3097
Jannatul Ferdous <ferdousmunmun75@gmail.com>
2026-06-21 07:16:07 +0600

    featuredsection added

---
+++ src/components/FeaturedSection.jsx
@@ -0,0 +1,41 @@
+<section className="py-20 bg-white">
+  <div className="max-w-7xl mx-auto px-6">
+    <h2 className="text-4xl font-bold text-center mb-12">
+      Why Choose PulseCare?
+    </h2>
+
+    <div className="grid md:grid-cols-3 gap-8">
+
+      <div className="p-8 rounded-2xl shadow-lg border">
+        <h3 className="text-2xl font-semibold mb-4">
+          Instant Donor Search
+        </h3>
+        <p className="text-gray-600">
+          Find blood donors instantly by blood group,
+          district and upazila.
+        </p>
+      </div>
+
+      <div className="p-8 rounded-2xl shadow-lg border">
+        <h3 className="text-2xl font-semibold mb-4">
+          Verified Donors
+        </h3>
+        <p className="text-gray-600">
+          Connect with registered and verified donors
+          to ensure safe blood donation.
+        </p>
+      </div>
+
+      <div className="p-8 rounded-2xl shadow-lg border">
+        <h3 className="text-2xl font-semibold mb-4">
+          Emergency Support
+        </h3>
+        <p className="text-gray-600">
+          Get urgent blood assistance during emergency
+          situations quickly and efficiently.
+        </p>
+      </div>
+
+    </div>
+  </div>
+</section>
