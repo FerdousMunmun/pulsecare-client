605fcf9d0ee7f67ee66f9ed74945d41e0e9f8749
Jannatul Ferdous <ferdousmunmun75@gmail.com>
2026-06-22 12:58:52 +0600

    Edit button create

---
+++ src/app/dashboard/donor/edit-donation-request/[id]/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function EditDonationRequestPage({
+  params,
+}) {
+  return (
+    <div>
+      <h1>Edit Request</h1>
+
+      <p>ID: {params.id}</p>
+    </div>
+  );
+}
